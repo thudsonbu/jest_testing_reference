ffb6bd8f1b23f94dfd6416ee37a0afef93b35d65
=hudson_thom@bentley.edu <hudson_thom@bentley.edu>
2020-12-26 19:13:48 -0500

    Object tests and regular expressions

--- src/functions.test.js
+++ src/functions.test.js
@@ -1,23 +1,39 @@
-const { expect } = require('@jest/globals');
-const functions = require('./functions');
+const { expect } = require("@jest/globals");
+const functions = require("./functions");
 
-test('Adds 2 + 2 = 4', () => {
-    expect(functions.add(2, 2)).toBe(4);
+test("Adds 2 + 2 = 4", () => {
+  expect(functions.add(2, 2)).toBe(4);
 });
 
-test('Adds 2 + 2 != 5', () => {
-    expect(functions.add(2, 2)).not.toBe(5);
+test("Adds 2 + 2 != 5", () => {
+  expect(functions.add(2, 2)).not.toBe(5);
 });
 
-test('Should be null', () => {
-    expect(functions.isNull()).toBeNull();
+test("Should be null", () => {
+  expect(functions.isNull()).toBeNull();
 });
 
-test('Should be falsy', () => {
-    expect(functions.checkValue(null)).toBeFalsy;
+test("Should be falsy", () => {
+  expect(functions.checkValue(null)).toBeFalsy;
 });
 
-test('Should be falsy', () => {
-    expect(functions.checkValue(null)).toBeFalsy;
+test("Should be falsy", () => {
+  expect(functions.checkValue(null)).toBeFalsy;
 });
 
+test("User should be Brad Traversy object", () => {
+  expect(functions.createUser()).toEqual({
+    firstName: "Brad",
+    lastName: "Traversy",
+  });
+});
+
+test("Should be under 1600", () => {
+  const load1 = 800;
+  const load2 = 700;
+  expect(load1 + load2).toBeLessThan(1600);
+});
+
+test('There is no I in team', () => {
+    expect('team').not.toMatch(/I/);
+});
